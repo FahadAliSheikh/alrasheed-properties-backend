2a02051f5a580e9f995903d4d3eb01647859642c
FahadAliSheikh <fahad.a.sheikh@gmail.com>
2022-12-23 18:27:57 +0500

    origin allowed for any

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,8 @@
 const allowedOrigins = [
-    'http://localhost:3000',
-    'https://www.fahadalisheikh.com',
-    'https://fahadalisheikh.com'
-
-]
+  // 'http://localhost:3000',
+  // 'https://www.fahadalisheikh.com',
+  // 'https://fahadalisheikh.com'
+  "*",
+];
 
 module.exports = allowedOrigins;

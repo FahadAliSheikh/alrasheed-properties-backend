8829fd788b260ed8c80699f4e4871198b1a2d8be
FahadAliSheikh <fahad.a.sheikh@gmail.com>
2022-12-31 19:58:17 +0500

    CORS allowed for all

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
   // 'http://localhost:3000',
   // 'https://www.fahadalisheikh.com',
   // 'https://fahadalisheikh.com'
-  "https://alrasheed-management-system.web.app/",
+  "https://alrasheed-management-system.web.app",
   "*",
 ];
 

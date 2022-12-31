9e55453b7f20e705bd526c77bafce06bf15f4d00
FahadAliSheikh <fahad.a.sheikh@gmail.com>
2022-12-31 19:36:12 +0500

    corst allowed for production

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   // 'http://localhost:3000',
   // 'https://www.fahadalisheikh.com',
   // 'https://fahadalisheikh.com'
+  "https://alrasheed-management-system.web.app/",
   "*",
 ];
 
